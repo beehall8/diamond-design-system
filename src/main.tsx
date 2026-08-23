a5c99671b31ee23037b91f48190c4a771fb2dd76
beehall8 <122220964+beehall8@users.noreply.github.com>
2026-08-23 19:24:59 -0400

    Route payment confirmation page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import PaymentConfirmed from './PaymentConfirmed'
 import './styles.css'
 
+const pathname = window.location.pathname.replace(/\/+$/, '') || '/'
+const Page = pathname === '/payment-confirmed' ? PaymentConfirmed : App
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode><Page /></React.StrictMode>,
 )
